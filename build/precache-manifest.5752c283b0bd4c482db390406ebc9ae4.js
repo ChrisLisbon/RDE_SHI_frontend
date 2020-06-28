cc1d7de33190505b05bd56755c053495361396c1
ChrisLisbon <yulashka.htm@yandex.ru>
2020-06-28 15:20:59 +0300

    Working version with full adaptive fonts

---
+++ build/precache-manifest.5752c283b0bd4c482db390406ebc9ae4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874aa9fab93c17ab9048d9c9cbb92055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afc63fb8d83fe84da655",
+    "url": "/static/css/2.98da02a2.chunk.css"
+  },
+  {
+    "revision": "4ca7ff697fd5d9e845c6",
+    "url": "/static/css/main.b7f85d1a.chunk.css"
+  },
+  {
+    "revision": "afc63fb8d83fe84da655",
+    "url": "/static/js/2.215eb14a.chunk.js"
+  },
+  {
+    "revision": "96b3fe6be44a6cdc0314e0ec388dffb7",
+    "url": "/static/js/2.215eb14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ca7ff697fd5d9e845c6",
+    "url": "/static/js/main.104c545f.chunk.js"
+  },
+  {
+    "revision": "928b9b0c619055fa9ae1",
+    "url": "/static/js/runtime-main.767f8cb0.js"
+  },
+  {
+    "revision": "af04d0557ff4a0c57e16d82eeacb96b8",
+    "url": "/static/media/shi_round.af04d055.png"
+  },
+  {
+    "revision": "c82cf08c6577c4bdd867f9da92a7c9b0",
+    "url": "/static/media/toggle.c82cf08c.svg"
+  },
+  {
+    "revision": "a06a3248f173f51bcbbcdd434a1de129",
+    "url": "/static/media/white-waves.a06a3248.png"
+  }
+]);
